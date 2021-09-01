08bd9d4e2081322d58745760c6fad406ba414eae
MirzayevOrzu <mirzayevorzu127@gmail.com>
2021-09-01 15:49:02 +0500

    configure logger middleware morgan

--- index.js
+++ index.js
@@ -3,9 +3,11 @@ const app = express();
 require('dotenv').config();
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const morgan = require('morgan');
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(morgan('dev'));
 
 const DB_URL = process.env.DB_URL;
 const PORT = process.env.PORT || 8080;
