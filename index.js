6893e212d2087f0444ac5e91d81b847b53f48108
MirzayevOrzu <mirzayevorzu127@gmail.com>
2021-09-01 12:16:28 +0500

    configure database connection

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
 const express = require('express');
 const app = express();
 require('dotenv').config();
+const mongoose = require('mongoose');
 
 
+const DB_URL = process.env.DB_URL;
 const PORT = process.env.PORT || 8080;
-app.listen(PORT, () => {
-  console.log(`Listening on port ${PORT}`);
-})
+mongoose.connect(DB_URL, (err) => {
+  if (err) {
+    console.log('Database connection error');
+  } else {
+    console.log('Database connection open');
+    app.listen(PORT, () => {
+      console.log(`Listening on port ${PORT}`);
+    });
+  }
+});
