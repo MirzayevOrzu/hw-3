818d77cbbf61b9da3fb65a5dc7198bed8def8300
MirzayevOrzu <mirzayevorzu127@gmail.com>
2021-09-10 18:36:36 +0500

    prevent deleting profile while active user

--- src/controllers/user-controllers.js
+++ src/controllers/user-controllers.js
@@ -40,6 +40,28 @@ module.exports.changePassword = async (req, res, next) => {
 module.exports.deleteProfile = async (req, res, next) => {
   const {id} = req.user;
 
+  const user = await User.findById(id);
+
+  if (user.role === 'shipper') {
+    const load = await Load
+        .findOne({assigned_to: {$ne: null}, created_by: user._id})
+        .select('-truck')
+        .lean();
+
+    if (load) {
+      return next(new ExpressError('You have active load, cannot delete', 400));
+    }
+  } else {
+    const load = await Load
+        .findOne({assigned_to: user._id, status: {$ne: 'SHIPPED'}})
+        .select('-truck')
+        .lean();
+
+    if (load) {
+      return next(new ExpressError('Active load found, cannot delete', 400));
+    }
+  }
+
   await User.findByIdAndRemove(id);
 
   res.status(200).json({
